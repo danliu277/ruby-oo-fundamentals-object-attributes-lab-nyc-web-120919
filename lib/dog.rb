df5a1330ab60ce3dafec0f6d2f04cbf44de279d0
Daniel Liu <danliu277@gmail.com>
2019-12-09 22:07:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,17 @@
 class Dog
+  def name=(name)
+    @name = name
+  end
+  
   def name
     @name
   end
   
-  def
+  def breed=(breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed = breed
+  end
+end
